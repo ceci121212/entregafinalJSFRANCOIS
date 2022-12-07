192e54de60675ee19e6ed886667e7415c9895edf
Cecilia <cecifrancois23@gmail.com>
2022-12-07 10:04:14 -0300

    agregué navbar

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-// class Producto {
-//   constructor(id, nombre, precio) {
-//     this.id = id
-//     this.nombre = nombre
-//     this.precio = precio
-//   }
-// }
- 
-// const producto1 = new Producto(1, 'Pañales', 600)
-// const producto2 = new Producto(2, 'Mamadera', 1000)
-// const producto3 = new Producto(3, 'Cuna', 2000)
-// const producto4 = new Producto(4, 'Mameluco', 800)
-
-const productosFetch = await fetch ('productos.json')
+fetch('./productos.json')
+.then (res => res.json ())
+.then (res => carrito)
 
 
-const productosArray = [
-  producto1,
-  producto2,
-  producto3,
-  producto4,
-]
+class Producto {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
 
+const producto1 = new Producto(1, "Pañales", 600);
+const producto2 = new Producto(2, "Mamadera", 1000);
+const producto3 = new Producto(3, "Cuna", 2000);
+const producto4 = new Producto(4, "Mameluco", 800);
 
-const divProductos = document.querySelector('#divProductos')
+const productosArray = [producto1, producto2, producto3, producto4];
+
+const divProductos = document.querySelector("#divProductos");
 
 productosArray.forEach((producto) => {
   divProductos.innerHTML += `
@@ -33,62 +29,58 @@ productosArray.forEach((producto) => {
     <button id="${producto.id}" class="btn btn-primary">Agregar</button>
     </div>
     </div>
-    `
-})
+    `;
+});
 
-
-const carrito = []
-const botonesAgregar = document.querySelectorAll('.btn-primary')
-const totalcompra = document.getElementById("total")
+const carrito = [];
+const botonesAgregar = document.querySelectorAll(".btn-primary");
+const totalcompra = document.getElementById("total");
 
 botonesAgregar.forEach((boton) => {
   boton.onclick = () => {
     const producto = productosArray.find(
       (prod) => prod.id === parseInt(boton.id)
-    )
+    );
 
     const productoCarrito = {
       id: producto.id,
       nombre: producto.nombre,
       precio: producto.precio,
       cantidad: 1,
-    }
+    };
 
-    const indexCarrito = carrito.findIndex((prod) => prod.id === producto.id)
+    const indexCarrito = carrito.findIndex((prod) => prod.id === producto.id);
 
     if (indexCarrito === -1) {
-      carrito.push(productoCarrito)
+      carrito.push(productoCarrito);
       const totalCompra = carrito
-    .map((prod) => prod.precio * prod.cantidad)
-    .reduce((elem1, elem2) => elem1 + elem2)
-    console.log(totalCompra)
-    localStorage.setItem("Total-compra", totalCompra)
+        .map((prod) => prod.precio * prod.cantidad)
+        .reduce((elem1, elem2) => elem1 + elem2);
+      console.log(totalCompra);
+      localStorage.setItem("Total-compra", totalCompra);
     } else {
-      carrito[indexCarrito].cantidad += 1
+      carrito[indexCarrito].cantidad += 1;
     }
-    console.log(carrito)
-  }
-})
+    console.log(carrito);
+  };
+});
 
-
-const totalDiv = document.getElementById('total')
-const informarTitulo = document.createElement ('h2')
+const totalDiv = document.getElementById("total");
+const informarTitulo = document.createElement("h2");
 
 const calcularTotalCompra = () => {
   let total = 0;
   carrito.forEach((producto) => {
     total += producto.precio * producto.cantidad;
   });
-  console.log(total)
-  informarTitulo.innerHTML = `El total de tu compra es de ${total} pesos.`
-  totalDiv.append(informarTitulo)
+  console.log(total);
+  informarTitulo.innerHTML = `El total de tu compra es de ${total} pesos.`;
+  totalDiv.append(informarTitulo);
 };
 
-
-const botonFinalizar = document.getElementById('finalizar')
-botonFinalizar.addEventListener ("click", () => {
-  divProductos.remove()
-  botonFinalizar.remove ()
-  calcularTotalCompra()
-  
-})
+const botonFinalizar = document.getElementById("finalizar");
+botonFinalizar.addEventListener("click", () => {
+  divProductos.remove();
+  botonFinalizar.remove();
+  calcularTotalCompra();
+});
